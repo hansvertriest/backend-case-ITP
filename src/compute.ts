5108f0259b43e51baa82916369cc4367300e6e88
Hans Vertriest <hans.vertriest@gmail.com>
2020-10-06 00:52:14 +0200

    works

--- src/compute.ts
+++ src/compute.ts
@@ -1,4 +1,66 @@
 import { Game, Frame, LastFrame } from "./types";
+import e from "express";
+
+const getStrikePoints = (game: Game, frameIndex: number) => {
+  const nextFrame : Frame | LastFrame = game[frameIndex+1];
+  let strikePoints:number = 10;
+  if (frameIndex < 9) {
+    if (nextFrame[0] == 10) {
+      // firstBall = strike
+      const firstBall: number = 10;
+      const secondBall : number = (frameIndex+2 <= 9)? game[frameIndex+2][0] : game[frameIndex + 1][1];
+      strikePoints += firstBall + secondBall;
+    } else {
+      // spare / normal
+      const firstBall: number = nextFrame[0];
+      const secondBall : number = nextFrame[1];
+      strikePoints += firstBall + secondBall;
+    }
+  } else {
+    strikePoints += game[frameIndex][0] + game[frameIndex][1] + game[frameIndex][2];
+  }
+
+  return strikePoints;
+}
+
+const getSparePoints = (game: Game, frameIndex: number) => {
+  const nextFrame : Frame | LastFrame = game[frameIndex+1];
+  let sparePoints:number = 10;
+  if (frameIndex < 9) {
+    if (nextFrame[0] == 10) {
+      // firstBall = strike
+      const firstBall: number = 10;
+      sparePoints += firstBall;
+    } else {
+      // spare / normal
+      const firstBall: number = nextFrame[0];
+      sparePoints += firstBall;
+    }
+  } else {
+    sparePoints += game[frameIndex][2];
+  }
+
+  return sparePoints;
+
+}
+
+const getFrameTotal = (game: Game, frameIndex: number) => {
+    const frame : Frame | LastFrame = game[frameIndex];
+    let frameTotal: number = 0;
+
+    if (frame[0] == 10) {
+      // strike
+      frameTotal += getStrikePoints(game, frameIndex);
+    } else if ( frame[0] + frame[1] == 10 ) {
+      // spare
+      frameTotal += getSparePoints(game, frameIndex);
+    } else {
+      frameTotal += frame[0] + frame[1];
+    }
+
+    return frameTotal;
+}
+
 
 export function compute(game: Game): number {
   // rules
@@ -7,23 +69,10 @@ export function compute(game: Game): number {
   let gameTotal: number = 0;
 
   game.forEach(( frame: Frame, index: number ) => {
-    let frameTotal: number = 0;
-    frameTotal += frame[0] + frame[1];
 
-    // add extra points
-    if (frame[0] === 10) {
-      // STRIKE 🚀
-      // frame == strike => 10 + frame(N+1)[0] + frame(N+1)[1]
-      frameTotal += game[index+1][0] + game[index+1][1]
+    let frameTotal: number = getFrameTotal(game, index);
 
-    } else if ( frame[0] + frame[1] == 10 ) {
-      // SPARE 🚀
-      // frame == spare => 10 + frame(N+1)[0]
-      frameTotal = (index < 9) 
-        ? frameTotal + game[index+1][0] 
-        : frameTotal + game[index][2];
-    }
-
+    console.log(frameTotal);
     // add frameTotal to gameTotal
     gameTotal += frameTotal;
   });
