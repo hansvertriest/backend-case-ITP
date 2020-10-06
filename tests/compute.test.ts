22643f39f410f98f409b1cdc11ae3cdc01792ed3
Hans Vertriest <hans.vertriest@gmail.com>
2020-10-06 10:52:56 +0200

    added extra tests

--- tests/compute.test.ts
+++ tests/compute.test.ts
@@ -18,4 +18,78 @@ it("should return 300 on a perfect game", () => {
   const score = compute(input300);
 
   expect(score).toBe(300);
+
+  const inputTripleStrike: Game = [
+    [10, 0],
+    [10, 0],
+    [10, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0, 0],
+  ]
+
+  expect(compute(inputTripleStrike)).toBe(60)
+
+});
+
+
+it("should return 60 on just three trikes", () => {
+  const inputTripleStrike: Game = [
+    [10, 0],
+    [10, 0],
+    [10, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0, 0],
+  ];
+
+  const score = compute(inputTripleStrike);
+
+  expect(score).toBe(60);
+});
+
+it("should return 88 on this set of frames", () => {
+  const inputTripleStrike: Game = [
+    [10, 0],
+    [10, 0],
+    [10, 0],
+    [2, 8],
+    [3, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0, 0],
+  ];
+
+  const score = compute(inputTripleStrike);
+
+  expect(score).toBe(88);
+});
+
+it("should return 135 on this set of frames", () => {
+  const inputTripleStrike: Game = [
+    [4, 5],
+    [10, 0],
+    [5, 5],
+    [6, 3],
+    [10, 0],
+    [0, 10],
+    [7, 2],
+    [3, 1],
+    [10, 0],
+    [2, 8, 1],
+  ];
+
+  const score = compute(inputTripleStrike);
+
+  expect(score).toBe(135);
 });
